297e3f467c8cfa5de821e122b3418aad3094d26d
AIME JOSEPH ANDRIANOMENTSOA <j.andria@ergosante.fr>
2023-03-07 10:22:36 +0300

    add signature-pad

--- projects/ng-signature/src/lib/ng-signature.component.ts
+++ projects/ng-signature/src/lib/ng-signature.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'ng-signature',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./ng-signature.scss'],
 })
 export class NgSignatureComponent {
-  
+  @Input() points: any[] = [];
+  @Output() pointsChange = new EventEmitter<any>();
+
+  @Input() editable = true;
+  @Input() emitOnDragEnd = false;
+
+  @Input() penColor = 'black';
+  @Input() backgroundColor = 'transparent';
+
+  @Input() showDoneButton = true;
+  @Input() doneButtonText = 'Done';
+  @Input() doneButtonClass = 'btn btn-primary';
+  @Input() showClearButton = true;
+  @Input() clearButtonText = 'Clear';
+  @Input() clearButtonClass = 'btn btn-default';
+
+  @Input() format: 'blob' | 'base64' | 'json' = 'blob';
+
+  @Input() height = 150;
+  @Input() width = 600;
+
+  @Output() done = new EventEmitter();
+  @Output() cleared = new EventEmitter();
+
+  @Input() responsive = true;
+
+  saveImage(blob: Blob | any) {
+    this.done.emit(blob);
+  }
 }
--- projects/ng-signature/src/lib/ng-signature.module.ts
+++ projects/ng-signature/src/lib/ng-signature.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgSignatureComponent } from './ng-signature.component';
-
-
+import { SignaturePadModule } from '@ng-plus/signature-pad';
 
 @NgModule({
-  declarations: [
-    NgSignatureComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    NgSignatureComponent
-  ]
+  declarations: [NgSignatureComponent],
+  imports: [SignaturePadModule],
+  exports: [NgSignatureComponent],
 })
-export class NgSignatureModule { }
+export class NgSignatureModule {
+}
